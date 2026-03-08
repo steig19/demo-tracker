d9521509b0ce78fe46dba837039ffdaeff09a41c
steig19 <steig@mac.com>
2026-03-08 17:05:02 -0500

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -765,7 +765,7 @@
             <div class="pct-row"><span>Northern California Section</span>1093.4 to 1720.4</div>
             <div class="pct-row"><span>Oregon Section</span>1720.4 to 2150.2</div>
             <div class="pct-row"><span>Washington Section</span>2150.2 to 2655.8</div>
-            <div class="pct-row"><b>Pacific Crest Trail Line</b><span style='color: #ffcc00; font-weight:bold;'>--------------</span></div>
+            <div class="pct-row"><b>Pacific Crest Trail Line</b><span style='color: #03B1FC; font-weight:bold;'>--------------</span></div>
           </div>
         </div>
       </div>
